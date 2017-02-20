b0512f84bb53c4e6738844b69e50c65886f387df
aadithpm <aadithpmdesigns@gmail.com>
2017-02-20 13:17:07 +0530

    Added ipinfo API code

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
+
 $(document).ready(function() {
 	
+	function getLoc()
+	{
+		$.getJSON('http://ipinfo.io', function(data) {
+  						
+  		console.log(data)
+		
+		});
+	}
 
+	var positionLat;
+	var positionLong;
+	getLoc();
 
 });
