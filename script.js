eadc14d9cdb47f10b81d38995659677c1f823a02
aadithpm <aadithpmdesigns@gmail.com>
2017-02-22 16:18:04 +0530

    Cleaned up jquery weather code

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ $(document).ready(function() {
 	$.getJSON('http://ipinfo.io', function(data) {
 			
 				var latAndLongStr;			
-				console.log(data);
+				//console.log(data);
 
 				$("#city").text(data.city);
 				$("#region").text(data.region);
 				$("#country").text(data.country);
 				var latAndLong = data.loc.split(',');
 				latAndLongStr = "Latitude: "+latAndLong[0]+" || "+"Longitude: "+latAndLong[1];
-				console.log(latAndLongStr);
+				//console.log(latAndLongStr);
 
 				lat = latAndLong[0];
 				long = latAndLong[1];
@@ -25,7 +25,7 @@ $(document).ready(function() {
 				var latAndLongDetails = $(".fa-question").jBox("Tooltip");
 				latAndLongDetails.setTitle(latAndLongStr).setContent("");
 
-			console.log(lat+"||"+long);
+			//console.log(lat+"||"+long);
 			$.ajax({
 				url: 'https://simple-weather.p.mashape.com/weatherdata?lat='+lat+"&"+"lng="+long,
 				headers:
@@ -35,14 +35,12 @@ $(document).ready(function() {
 				},
 				success:function(response)
 				{
-					var weather=JSON.parse(response);
-					//var forecast=weather.query.results.channel.item.forecast[0];
-					console.log(response);
+					var forecast=JSON.parse(response).query.results.channel.item.forecast[0];
 					//console.log(forecast);
-					$("#date-and-day").text(weather.query.results.channel.item.forecast[0].day+", "+weather.query.results.channel.item.forecast[0].date);
-					$("#degree-low").text(weather.query.results.channel.item.forecast[0].low);
-					$("#degree-high").text(weather.query.results.channel.item.forecast[0].high);
-					$("#weather-type").text(weather.query.results.channel.item.forecast[0].text);
+					$("#date-and-day").text(forecast.day+", "+forecast.date);
+					$("#degree-low").text(forecast.low);
+					$("#degree-high").text(forecast.high);
+					$("#weather-type").text(forecast.text);
 				}
 				
 			});
